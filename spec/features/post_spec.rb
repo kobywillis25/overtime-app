b34edb379e3123dbfd39000a2c51f6dd1d2b844f
Koby Willis <koby_willis@hotmail.com>
2019-05-22 13:13:18 -0600

    Implemented test and page redirection for permission structer

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -7,7 +7,7 @@ describe 'navigate' do
   end
 
   describe 'index' do
-    before do 
+    before do
       visit posts_path
     end
 
@@ -23,17 +23,17 @@ describe 'navigate' do
       post1 = FactoryBot.build_stubbed(:post)
       post2 = FactoryBot.build_stubbed(:second_post)
       visit posts_path
-      expect(page).to have_content(/Rationale|Content/)
+      expect(page).to have_content(/Rationale|content/)
     end
   end
 
   describe 'new' do
     it 'has a link from the homepage' do
-    visit root_path
+      visit root_path
 
-        click_link("new_post_from_nav")
-    expect(page.status_code).to eq(200)
-   end
+      click_link("new_post_from_nav")
+      expect(page.status_code).to eq(200)
+    end
   end
 
   describe 'delete' do
@@ -41,8 +41,8 @@ describe 'navigate' do
       @post = FactoryBot.create(:post)
       visit posts_path
 
-    click_link("delete_post_#{@post.id}_from_index")
-    expect(page.status_code).to eq(200)
+      click_link("delete_post_#{@post.id}_from_index")
+      expect(page.status_code).to eq(200)
     end
   end
 
@@ -73,25 +73,30 @@ describe 'navigate' do
   end
 
   describe 'edit' do
-    before do 
-      @post = FactoryBot.create(:post)
+    before do
+      @edit_user = User.create(first_name: "asdf", last_name: "asdf", email: "asdfasdf@asdf.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      login_as(@edit_user, :scope => :user)
+      @edit_post = Post.create(date: Date.today, rationale: "asdf", user_id: @edit_user.id)
     end
 
-    it 'can be reached by clicking edit on index page' do 
-      visit posts_path
-
-      click_link("edit_#{@post.id}")
-      expect(page.status_code).to eq(200)
-  end
-
-  it 'can be edited' do 
-    visit edit_post_path(@post)
+    it 'can be edited' do
+      visit edit_post_path(@edit_post)
 
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Edited content"
       click_on "Save"
 
       expect(page).to have_content("Edited content")
+    end
+
+    it 'cannot be edited by a non authorized user' do
+      logout(:user)
+      non_authorized_user = FactoryBot.create(:non_authorized_user)
+      login_as(non_authorized_user, :scope => :user)
+
+      visit edit_post_path(@edit_post)
+
+      expect(current_path).to eq(root_path)
+    end
   end
- end
 end
